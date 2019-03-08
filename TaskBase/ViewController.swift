7c98594b2ec5fc0b5b4c28d7cb18dce48a6a71e7
nobezawa <charmy.nobezawa@gmail.com>
2019-03-08 16:30:53 +0900

    Initial Commit

---
+++ TaskBase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TaskBase
+//
+//  Created by 延澤拓郎 on 2019/03/08.
+//  Copyright © 2019 延澤拓郎. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
