fadafa4b2a2df3d3f423067ff586e54ac002f6c3
nobezawa <charmy.nobezawa@gmail.com>
2019-03-09 17:42:54 +0900

    add TabBarController

---
+++ TaskBase/MainTabBarController.swift
@@ -0,0 +1,41 @@
+//
+//  MainTabBarController.swift
+//  TaskBase
+//
+//  Created by 延澤拓郎 on 2019/03/09.
+//  Copyright © 2019 延澤拓郎. All rights reserved.
+//
+
+import UIKit
+
+final class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        var controllers = [UIViewController]()
+        let taskListViewController = TaskListViewController()
+        taskListViewController.tabBarItem = UITabBarItem(tabBarSystemItem: UITabBarItem.SystemItem.bookmarks, tag: 1)
+        controllers.append(taskListViewController)
+        
+        let searchListViewController = SearchListViewController()
+        searchListViewController.tabBarItem = UITabBarItem(tabBarSystemItem: UITabBarItem.SystemItem.search, tag: 2)
+        controllers.append(searchListViewController)
+
+
+        self.setViewControllers(controllers, animated: false)
+        self.selectedIndex = 1
+        self.selectedIndex = 0
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
