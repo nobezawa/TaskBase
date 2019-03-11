3bced18d3e3e56efd18f18c607756bf78ff655b6
nobezawa <charmy.nobezawa@gmail.com>
2019-03-11 18:04:40 +0900

    display category tableview

--- TaskBase/Search/FilterCategoryViewController.swift
+++ TaskBase/Search/FilterCategoryViewController.swift
@@ -8,12 +8,26 @@
 
 import UIKit
 
-class FilterCategoryViewController: UIViewController {
+class FilterCategoryViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBAction func cancelButton(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    let categoryList = FilterCategory.list()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categoryList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "FilterCategoryCell", for: indexPath)
+        cell.textLabel?.text = categoryList[indexPath.row].name
+        return cell
     }
 
 }
