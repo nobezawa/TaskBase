6daa2756d915b38aba3c3626be4ed3714c440b9f
nobezawa <charmy.nobezawa@gmail.com>
2019-03-27 12:40:15 +0900

    fix display category selected

--- TaskBase/Search/FilterCategoryViewController.swift
+++ TaskBase/Search/FilterCategoryViewController.swift
@@ -30,19 +30,18 @@ final class FilterCategoryViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        guard let viewModel = self.viewModel else { return }
 
         let dataSource = RxTableViewSectionedReloadDataSource<SectionCategory>(
             configureCell: {[weak self] dataSource, tableView, indexPath, item in
                 let cell: FilterCategoryTableViewCell = tableView.dequeueReusableCell(withIdentifier: self!.cellId, for: indexPath) as! FilterCategoryTableViewCell
                 cell.titleLabel.text = item.name
-                cell.doneImageView.image = UIImage(named: "done")
+                cell.doneImageView.image = item.selected ? UIImage(named: "done") : UIImage(named: "not_done")
                 return cell
             }
         )
 
-        guard let viewModel = self.viewModel else { return }
-
-        viewModel.categories?
+        viewModel.categories
             .bind(to: categoryTableView.rx.items(dataSource: dataSource))
             .disposed(by: disposeBag)
 
@@ -54,6 +53,7 @@ final class FilterCategoryViewController: UIViewController {
             .subscribe(onNext: { [weak self] indexPath in
                 let category = viewModel.categoryList[indexPath.row]
                 viewModel.filterCategory(category: category)
+                viewModel.updateCategoryView(category: category)
                 self?.dismiss(animated: true, completion: nil)
             })
             .disposed(by: disposeBag)
--- TaskBase/Search/ViewModel/FilterCategoryViewModel.swift
+++ TaskBase/Search/ViewModel/FilterCategoryViewModel.swift
@@ -5,6 +5,7 @@
 
 import UIKit
 import RxSwift
+import RxCocoa
 import Differentiator
 
 struct SectionCategory {
@@ -21,17 +22,50 @@ extension SectionCategory: SectionModelType {
 }
 
 final class FilterCategoryViewModel: SearchViewModel {
-    let categoryList: [CategoryModel] = [WorkCategory(), LifeCategory()]
-    var categories: Observable<[SectionCategory]>?
+    var categoryList: [CategoryModel] = [WorkCategory(), LifeCategory()].sorted(by: {$0.id < $1.id})
+    var categories: BehaviorRelay<[SectionCategory]> = BehaviorRelay(value: [])
     let height: Observable<CGFloat> = Observable.just(CGFloat(50 * 2))
+    private let disposeBag = DisposeBag()
 
     required init(mediator: SearchTaskMediatorProtocol) {
         super.init(mediator: mediator)
 
-        self.categories = Observable.just([SectionCategory(items: self.categoryList)])
+        self.categories.accept([SectionCategory(items: self.categoryList)])
     }
 
     func filterCategory(category: CategoryModel) {
         mediator.filterTasks(category: category)
+    }
+
+    func updateCategoryView(category: CategoryModel) {
+        let reverseSelected = {(category: CategoryModel) -> CategoryModel in
+            var copyCategory = category
+            copyCategory.selected = !copyCategory.selected
+            return copyCategory
+        }
+
+        let unCheckSelected = {(category: CategoryModel) -> CategoryModel in
+            var copyCategory = category
+            copyCategory.selected = false
+            return copyCategory
+        }
+
+        let reverseSubscription = Observable
+            .from(categoryList)
+            .filter { $0.id == category.id }
+            .map { reverseSelected($0) }
+
+        let unCheckSubscription = Observable
+            .from(categoryList)
+            .filter { $0.id != category.id }
+            .map { unCheckSelected($0) }
+
+        Observable
+            .combineLatest(reverseSubscription, unCheckSubscription) { [$0, $1] }
+            .subscribe(onNext: {[weak self] (categoryList: [CategoryModel]) in
+                guard let self = self else { return }
+                self.categories.accept([SectionCategory(items: categoryList.sorted(by: {$0.id < $1.id}))])
+            })
+            .disposed(by: disposeBag)
     }
 }
