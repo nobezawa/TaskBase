2b2d5d837f730ef0d7c8b95b60c2853bae94a8ae
nobezawa <charmy.nobezawa@gmail.com>
2019-03-29 17:02:56 +0900

    remove unused code

--- TaskBase/MyTask/MyTodo.swift
+++ TaskBase/MyTask/MyTodo.swift
@@ -30,11 +30,3 @@ final class ReMyTodo: Object {
         }
     }
 }
-
-class DemoMyTodo {
-    static func sample() -> [MyTodo] {
-        let todo1 = MyTodo(id: "1", title: "経費計算をする", finished: false)
-        let todo2 = MyTodo(id: "2", title: "ふるさと納税の金額を計算をする", finished: false)
-        return [todo1, todo2]
-    }
-}
